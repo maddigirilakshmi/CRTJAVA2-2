0a0f05eacb80b2316e2778ec4bab5b08dfb0aaab
maddigirilakshmi <155134883+maddigirilakshmi@users.noreply.github.com>
2024-01-05 16:35:02 +0530

    Add files via upload

---
+++ Test13.java
@@ -0,0 +1,21 @@
+class First
+{
+void show(double...a)
+{
+    System.out.println("var arg method");
+}
+void show(double x,double y)
+{
+    System.out.println("two arg double method");
+}
+}
+class Test13
+{
+    public static void main(String[] args)
+    {
+        First f1=new First();
+        f1.show(345.9,987.9);
+        f1.show();
+        f1.show(23,56,985,89,8765,89,69,49,48,50,68,965);
+    }
+}
